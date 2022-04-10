bbf86e6c4ef620dd8a30c3d211d6836a7e46d662
bodqaua <bodqaua@gmail.com>
2022-04-10 14:12:05 +0300

    initial commit

---
+++ public/sw.js
@@ -0,0 +1,13 @@
+self.addEventListener("install", (event) => {
+    console.log("Service Workers installing... ", event);
+});
+
+self.addEventListener("activate", (event) => {
+    console.log("Activating Service Workers ", event);
+    return self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+        // console.log(`Fetching: ${event.request.url}`);
+});
+
